8b433ba1ad30ec028054fccf006beb9f68c9c6ac
Richard Spitz <rspitz@microsoft.com>
2019-01-05 11:30:46 -0800

    Create Cleanup.ps1

---
+++ Cleanup.ps1
@@ -0,0 +1,50 @@
+param
+(
+    [Parameter (Mandatory = $false)]
+    [object] $WebhookData
+)
+
+
+
+# If runbook was called from Webhook, WebhookData will not be null.
+if ($WebhookData) {
+
+    # Check header for message to validate request
+    if ($WebhookData.RequestHeader.message -eq 'ImageCopy')
+    {
+        Write-Output "Header has required information"}
+    else
+    {
+        Write-Output "Header missing required information";
+        exit;
+    }
+
+    # Retrieve VM's from Webhook request body
+    $rgs = (ConvertFrom-Json -InputObject $WebhookData.RequestBody)
+
+    # Authenticate to Azure by using the service principal and certificate. Then, set the subscription.
+
+    Write-Output "Authenticating to Azure with service principal and certificate"
+    $ConnectionAssetName = "AzureRunAsConnection"
+    Write-Output "Get connection asset: $ConnectionAssetName"
+
+    $Conn = Get-AutomationConnection -Name $ConnectionAssetName
+            if ($Conn -eq $null)
+            {
+                throw "Could not retrieve connection asset: $ConnectionAssetName. Check that this asset exists in the Automation account."
+            }
+            Write-Output "Authenticating to Azure with service principal." 
+            Add-AzureRmAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint | Write-Output
+
+        
+        # Start each virtual machine
+        foreach ($rg in $rgs)
+        {
+            $rgToRemove = $rg.Name
+            Remove-AzureRmResourceGroup -Name $rgToRemove -Force -Verbose
+        }
+}
+else {
+    # Error
+    write-Error "This runbook is meant to be started from an Azure webhook only."
+}
